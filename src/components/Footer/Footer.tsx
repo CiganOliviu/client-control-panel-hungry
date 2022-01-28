7ecc1e6d41f9b6c89e92a410a7d17d2097a4a9ca
Cigan Oliviu David <ciganoliviudavid@gmail.com>
2022-01-28 21:42:42 +0200

    CCH-1 | Setup app iteration 1

---
+++ src/components/Footer/Footer.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { FooterContainer } from "./Footer.css";
+
+function Footer() {
+
+    return (
+        <FooterContainer>
+            <p>Header</p>
+        </FooterContainer>
+    )
+}
+
+export default Footer;
---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { HeaderContainer } from "./Header.css";
+
+function Header(){
+
+    return (
+        <HeaderContainer>
+            <p>Header</p>
+        </HeaderContainer>
+    )
+}
+
+export default Header;
