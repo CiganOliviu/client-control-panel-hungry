2b3a9c03339b25e863c5ae0772ed0f377de2b7c0
Cigan Oliviu David <ciganoliviudavid@gmail.com>
2022-03-04 00:26:59 +0200

    CCH-5 | Setup environment

--- src/pages/CountriesPage/CountriesPage.tsx
+++ src/pages/CountriesPage/CountriesPage.tsx
@@ -2,15 +2,16 @@ import React from "react";
 import Footer from "../../components/Footer/Footer";
 import Header from "../../components/Header/Header";
 
-function MainPage() {
+function CountriesPage() {
 
     return (
         <div>
             <Header />
             <h1>Hungry Control Center</h1>
+            <p>Countries Page</p>
             <Footer />
         </div>
     )
 }
 
-export default MainPage;
+export default CountriesPage;
---
+++ src/pages/SchemasPage/SchemasPage.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import Footer from "../../components/Footer/Footer";
+import Header from "../../components/Header/Header";
+
+function SchemasPage() {
+
+    return (
+        <div>
+            <Header />
+            <h1>Hungry Control Center</h1>
+            <Footer />
+        </div>
+    )
+}
+
+export default SchemasPage;
