b166f9dd81c32311937a80b664aeab20eb85e9ea
Cigan Oliviu David <ciganoliviudavid@gmail.com>
2022-03-04 00:10:18 +0200

    HOTFIX: Setup app arhitecture

---
+++ src/backendApi/requestUrls.ts
@@ -0,0 +1,7 @@
+const backendUrl = 'http://localhost:8080';
+
+export const requestUrls = {
+    fetchCountries: `${backendUrl}/countries/`,
+    fetchUsers: `${backendUrl}/users/`,
+    fetchRecipes: `${backendUrl}/recipes/`,
+}
